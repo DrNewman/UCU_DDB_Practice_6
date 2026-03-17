9322c099a2a437415f5aad2dc747a4f37636395c
Oleksandr <dr.newman.ua@gmail.com>
2026-03-18 00:01:59 +0200

    Exercise 5 implementation

--- src/main/java/ucu/ddb/practice/ServerController.java
+++ src/main/java/ucu/ddb/practice/ServerController.java
@@ -1,48 +1,43 @@
 package ucu.ddb.practice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.*;
 
 @RestController
 public class ServerController {
 
     @Autowired
-    private MongoTemplate mongoTemplate;
+    private CassandraOperations cassandraOperations;
 
-    private final String COLLECTION = "counters";
     private final String COUNTER_ID = "web_hits";
-
-    private static final Logger log = LoggerFactory.getLogger(ServerController.class);
 
     @PostMapping("/reset")
     public String resetCounter() {
-        Query query = new Query(Criteria.where("_id").is(COUNTER_ID));
-        Update update = new Update().set("value", 0);
-        mongoTemplate.upsert(query, update, COLLECTION);
+        cassandraOperations.execute(deleteFrom("hits_counter")
+                .whereColumn("counter_id").isEqualTo(literal(COUNTER_ID))
+                .build());
         return "OK";
     }
 
     @PostMapping("/inc")
     public String inc() {
-        Query query = new Query(Criteria.where("_id").is(COUNTER_ID));
-        Update update = new Update().inc("value", 1);
-        mongoTemplate.updateFirst(query, update, COLLECTION);
+        cassandraOperations.execute(update("hits_counter")
+                .increment("value", literal(1))
+                .whereColumn("counter_id").isEqualTo(literal(COUNTER_ID))
+                .build());
         return "OK";
     }
 
     @GetMapping("/count")
-    public int count() {
-        Map res = mongoTemplate.findById(COUNTER_ID, Map.class, COLLECTION);
-        return res != null ? (Integer) res.get("value") : 0;
+    public long count() {
+        Long val = cassandraOperations.selectOne(
+                selectFrom("hits_counter").column("value")
+                        .whereColumn("counter_id").isEqualTo(literal(COUNTER_ID))
+                        .build(), Long.class);
+        return val != null ? val : 0;
     }
 }
